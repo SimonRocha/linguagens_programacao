94d4e215586fbd39de96d7516b694a5bcfa163cb
SimonRocha <simon.henrique.rocha@gmail.com>
2018-05-01 11:39:01 -0300

    CRUDS

--- regras/Inserir.py
+++ regras/Inserir.py
@@ -1,5 +1,5 @@
-from linguagens_programacao.modelo import Paciente
-from linguagens_programacao.modelo.dao import PacienteDAO
+from linguagens_programacao.modelo import Paciente, Administrador, Auxiliar, Atendente, Medico, Consulta
+from linguagens_programacao.modelo.dao import PacienteDAO, AdministradorDAO, AuxiliarDAO, AtendenteDAO, ConsultaDAO, MedicoDAO
 
 def inserirPaciente(nome, documento, dt_nascimento, dt_entrada):
     p = Paciente(999, nome, documento, dt_nascimento, dt_entrada)
@@ -12,21 +12,10 @@ def inserirPaciente(nome, documento, dt_nascimento, dt_entrada):
         return 0
 
 
-def inserirAdministrador(nome, documento, dt_nascimento, dt_entrada):
-    p = Paciente(999, nome, documento, dt_nascimento, dt_entrada)
+def inserirAdministrador(id, login, senha, tipo, nome, documento):
+    p = Administrador(999, login, senha, tipo, nome, documento)
 
-    p = PacienteDAO.inserir(p)
-
-    if (p != None):
-        return 1
-    else:
-        return 0
-
-
-def inserirPaciente(nome, documento, dt_nascimento, dt_entrada):
-    p = Paciente(999, nome, documento, dt_nascimento, dt_entrada)
-
-    p = PacienteDAO.inserir(p)
+    p = AdministradorDAO.inserir(p)
 
     if (p != None):
         return 1
@@ -34,21 +23,10 @@ def inserirPaciente(nome, documento, dt_nascimento, dt_entrada):
         return 0
 
 
-def inserirPaciente(nome, documento, dt_nascimento, dt_entrada):
-    p = Paciente(999, nome, documento, dt_nascimento, dt_entrada)
+def inserirAuxiliar(login, senha, tipo, nome, documento, funcao):
+    p = Auxiliar(999, login, senha, tipo, nome, documento, funcao)
 
-    p = PacienteDAO.inserir(p)
-
-    if (p != None):
-        return 1
-    else:
-        return 0
-
-
-def inserirPaciente(nome, documento, dt_nascimento, dt_entrada):
-    p = Paciente(999, nome, documento, dt_nascimento, dt_entrada)
-
-    p = PacienteDAO.inserir(p)
+    p = AuxiliarDAO.inserir(p)
 
     if (p != None):
         return 1
@@ -56,10 +34,32 @@ def inserirPaciente(nome, documento, dt_nascimento, dt_entrada):
         return 0
 
 
-def inserirPaciente(nome, documento, dt_nascimento, dt_entrada):
-    p = Paciente(999, nome, documento, dt_nascimento, dt_entrada)
+def inserirAtendente(login, senha, tipo, nome, documento):
+    p = Atendente(999, login, senha, tipo, nome, documento)
 
-    p = PacienteDAO.inserir(p)
+    p = AtendenteDAO.inserir(p)
+
+    if (p != None):
+        return 1
+    else:
+        return 0
+
+
+def inserirMedico(login, senha, tipo, nome, documento, especializacao):
+    p = Medico(999, login, senha, tipo, nome, documento, especializacao)
+
+    p = MedicoDAO.inserir(p)
+
+    if (p != None):
+        return 1
+    else:
+        return 0
+
+
+def inserirConsulta(nome,  usuarioRegistro, medico, paciente, auxiliares, dt_registro, dt_consulta, status):
+    p = Consulta(999, nome,  usuarioRegistro, medico, paciente, auxiliares, dt_registro, dt_consulta, status)
+
+    p = ConsultaDAO.inserir(p)
 
     if (p != None):
         return 1
